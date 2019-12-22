5e29a6b081b91ba91114fa6c11607f2fc5ab01ad
Toumette <58706169+Toumette@users.noreply.github.com>
2019-12-22 14:42:31 +0100

    Update COMPRESSER.bat

--- TRAITEMENT/COMPRESSER.bat
+++ TRAITEMENT/COMPRESSER.bat
@@ -7,33 +7,33 @@ echo ***************************************************************************
 echo *                    DEBUT DU TRAITEMENT - PATIENTER                       *
 echo ****************************************************************************
 
-rem bip de début de traitement :
+REM bip de début de traitement :
 echo 
-rem NB : le bip correspond au caractère ASCII 7 passé en argument à echo
-rem il peut être inséré sous Windows par la combinaison Alt007
+REM NB : le bip correspond au caractère ASCII 7 passé en argument à echo
+REM il peut être inséré sous Windows par la combinaison Alt007
 
-rem ----------------------------------------------------------------------------
-rem OPTIONS DE ffmpeg UTILISEES ICI :
-rem format conteneur : flv    (=extension de sortie) => nécessaire pour site/SPIP...
-rem codec vidéo      : h.264 -c:v libx264
-rem codec audio      : AAC   -c:a libvo_aacenc
-rem format video     : -s 640X360
+REM ----------------------------------------------------------------------------
+REM OPTIONS DE ffmpeg UTILISEES ICI :
+REM format conteneur : flv    (=extension de sortie) => nécessaire pour site/SPIP...
+REM codec vidéo      : h.264 -c:v libx264
+REM codec audio      : AAC   -c:a libvo_aacenc
+REM format video     : -s 640X360
 
-rem CONVERSION DE in.mp4 en out.flv
-rem pour mise sur site WEB/SPIP du Collège de films
-rem ----------------------------------------------------------------------------
+REM CONVERSION DE in.mp4 en out.flv
+REM pour mise sur site WEB/SPIP du Collège de films
+REM ----------------------------------------------------------------------------
 
-rem décommenter pour faire un test rapide sur 30s de la vidéo :
-rem ..\Programmes\bin\ffmpeg  -ss 60 -t 30 -i in.mp4 -c:v libx264 -c:a libvo_aacenc -s 640x360 out.flv
+REM décommenter pour faire un test rapide sur 30s de la vidéo :
+REM ..\Programmes\bin\ffmpeg -ss 60 -t 30 -i in.mp4 -c:v libx264 -c:a libvo_aacenc -s 640x360 out.flv
 
-rem si test OK :
-rem décommenter pour traiter entièrement la vidéo :
-..\Programmes\bin\ffmpeg                   -i in.mp4 -c:v libx264 -c:a libvo_aacenc -s 640x360 out.flv
+REM si test OK :
+REM décommenter pour traiter entièrement la vidéo :
+..\Programmes\bin\ffmpeg -i in.mp4 -c:v libx264 -c:a libvo_aacenc -s 640x360 out.flv
 
 echo.
 echo ----------------------- INFOS SUR LE FICHIER ENTREE -----------------------
 echo.
-..\Programmes\bin\ffmpeg -i  in.mp4
+..\Programmes\bin\ffmpeg -i in.mp4
 echo.
 
 echo ----------------------- INFOS SUR LE FICHIER SORTIE -----------------------
@@ -45,8 +45,10 @@ echo ***************************************************************************
 echo *  TRAITEMENT TERMINE - APPUYER SUR N'IMPORTE QUELLE TOUCHE POUR QUITTER   *
 echo ****************************************************************************
 
-rem 3 bips de fin de traitement :
+REM 3 bips de fin de traitement :
 echo  & echo  & echo 
 
-rem la commande "pause" permet d'attendre l'action sur une touche quelconque pour fermer la fenêtre
+REM la commande "pause" permet d'attendre l'action sur une touche quelconque pour fermer la fenêtre et
+REM le ">nul" évite l'affichage automatique de "Appuyez sur une touche pour continuer" car on a déjà
+REM affiché le message ci-dessus
 pause >nul

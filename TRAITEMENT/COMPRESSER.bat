ff3c5fd2d65d5b9f78971384e6c68bd6e28496fb
Toumette <58706169+Toumette@users.noreply.github.com>
2020-07-06 14:32:47 +0200

    Update COMPRESSER.bat

--- TRAITEMENT/COMPRESSER.bat
+++ TRAITEMENT/COMPRESSER.bat
@@ -2,9 +2,16 @@
 
 TITLE "TRAITEMENT DE LA VIDEO POUR LE SITE SPIP DU COLLEGE © Henri"
 
+REM ****************************************************************************
+REM Plus d'informations sur : https://henri.nitnoc.me
+REM Dernière version de l'outil et de sa documentation à télécharger sur :
+REM https://github.com/toumette => rubrique : CompressVideoSiteWeb
+REM © Henri
+REM ****************************************************************************
+
 echo.
 echo ****************************************************************************
-echo *                    DEBUT DU TRAITEMENT - PATIENTER                       *
+echo *               DEBUT DU TRAITEMENT - VEUILLEZ PATIENTER...                *
 echo ****************************************************************************
 
 REM bip de début de traitement :
@@ -13,14 +20,23 @@ REM NB : le bip correspond au caractère ASCII 7 passé en argument à echo
 REM il peut être inséré sous Windows par la combinaison Alt007
 
 REM ----------------------------------------------------------------------------
+REM tempo 3 secondes (inutilisée)
+REM TIMEOUT /T 3 /NOBREAK
+
+REM ----------------------------------------------------------------------------
 REM OPTIONS DE ffmpeg UTILISEES ICI :
-REM format conteneur : flv    (=extension de sortie) => nécessaire pour site/SPIP...
-REM codec vidéo      : h.264 -c:v libx264
-REM codec audio      : AAC   -c:a aac
-REM format video     : -s 640X360
+REM nom fich entrée  : in.mp4: -i in.mp4
+REM format conteneur : flv   : =extens fich sortie "out.flv" => nécessaire pour site/SPIP...
+REM codec vidéo      : h.264 : -c:v libx264
+REM codec audio      : AAC   : -c:a aac
+REM format video     : hXv   : -s 640X360
 
 REM CONVERSION DE in.mp4 en out.flv
-REM pour mise sur site WEB/SPIP du Collège de films
+REM pour mise sur site WEB/SPIP du Collège JB de films
+REM NB : si problème de conversion :
+REM mesurer la définition originale ex. 1920X 1080
+REM la réduire en 640 X (valeur à calculer pour conserver le même rapport H/V)
+REM par exemple 1,7777777777 soit 640X360
 REM ----------------------------------------------------------------------------
 
 REM décommenter pour faire un test rapide sur 30s de la vidéo :
